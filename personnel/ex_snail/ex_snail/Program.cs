24da664da85a2b32205de517b1b6696a70b3147f
pw52lvf <pw52lvf@eduvaud.ch>
2025-08-29 12:10:50 +0200

    ex escargot

---
+++ personnel/ex_snail/ex_snail/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ex_snail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAlive = true;
+            int x = 0;
+            int y = 10;
+
+
+            while (isAlive)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine("_@_ö");
+                x++;
+                Thread.Sleep(10);
+
+                if (x  == 50)
+                {
+                    break;
+                }
+            }
+            Console.Clear();
+            Console.SetCursorPosition((int)x, (int)y);
+            Console.WriteLine("____");
+        }
+    }
+}
